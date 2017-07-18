26dba337f073ebedc595ed7bca7c54e9a12bef30
hellraiserrob <robertjamesphillips@googlemail.com>
2017-07-18 10:11:36 +0100

    serve frontend from node app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React contentful
 
 A simple react / node app that gets and displays data from contentful
+
+https://whispering-bayou-69244.herokuapp.com
--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ var express = require('express')
 var logger = require('morgan')
 var helmet = require('helmet')
 var brands = require('./routes/brands')
+var path = require('path')
 var app = express()
 var port = process.env.PORT || 5000;
 
 
 app.use(helmet()) // protect from well known vulnerabilities
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
 app.use('/api/brands', brands)
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`App listening on port ${port}!`)
 })
 
 // catch 404 and forward to error handler
